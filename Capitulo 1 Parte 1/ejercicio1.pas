27f6c93edcc0d77b613fd46e52dc73e997542134
chapuwu <lolochapa10@hotmail.com>
2024-07-30 22:55:20 -0300

    first commit con primer ejercicio

---
+++ Capitulo 1 Parte 1/ejercicio1.pas
@@ -0,0 +1,22 @@
+program ejercicio1;
+
+var
+  numero1, numero2: integer;
+
+begin
+  // Leer los dos números desde el teclado
+  write('Introduce el primer número: ');
+  readln(numero1);
+  
+  write('Introduce el segundo número: ');
+  readln(numero2);
+  
+  // Comparar los números y mostrar el resultado
+  if numero1 > numero2 then
+    writeln('El primer número es mayor que el segundo número.')
+  else if numero1 < numero2 then
+    writeln('El segundo número es mayor que el primer número.')
+  else
+    writeln('Los números leídos son iguales.');
+  
+end.
