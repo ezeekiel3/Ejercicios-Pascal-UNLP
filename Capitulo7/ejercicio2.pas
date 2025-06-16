276f7ecf189304845c5c2d2c51a5619c9130df66
chapuwu <lolochapa10@hotmail.com>
2025-06-16 10:52:59 -0300

    falta completar el ejercicio2

---
+++ Capitulo7/ejercicio2.pas
@@ -0,0 +1,14 @@
+program ejercicio2;
+type
+    Cliente = record
+        codigoCliente: string;
+        dni: string;
+        apellido: string;
+        nombre: string;
+        codigoPoliza: integer;
+        montoBasico: integer;
+    end;
+
+begin
+    
+end;
