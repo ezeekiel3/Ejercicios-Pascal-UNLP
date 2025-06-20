71efcfb88911f6bdf90460fa9fc0c1e393af191b
chapuwu <lolochapa10@hotmail.com>
2025-06-20 18:47:26 -0300

    haciendo el ejercicio3

--- Capitulo7/ejercicio3.pas
+++ Capitulo7/ejercicio3.pas
@@ -8,14 +8,63 @@ type
         kms: integer;
     end;
 
+    AutoKm = record
+        codigoAuto: integer;
+        kms: integer;
+    end;
+
+    ListaAutoKm = ^nodo2
+    nodo2 = record
+        val: AutoKm;
+        sig: ListaAutoKm;
+    end;
+
     ListaViajes = ^nodo;
     nodo = record
         viaje: Viaje;
         sig: ListaViajes;
     end;
+procedure cargarViajes(var viajes: ListaViajes; viaje: Viaje);
+var
+    aux: ListaViajes;
+begin
+   new(aux);
+
+    aux^.viaje:= viaje;
+    aux^.sig:= viajes;
+
+    viajes:= aux;
+end;
+procedure autosQueMasRecorrieron(viajes: ListaViajes; var listaAutos: ListaAutoKm);
+var
+    aux: ListaViajes;
+    autoObjeto: AutoKm;
+    aux2: ListaAutoKm;
+begin
+    aux:= viajes;
+    while (aux <> nil) do begin
+        autoObjeto.codigoAuto:= aux^.viaje.codigoAuto;
+        autoObjeto.kms:= aux^.viaje.kms;
+        if (listaAutos = nil) then begin
+            new(aux2);
+
+            aux2^.val:= autoObjeto;
+            aux2^.sig:= listaAutos;
+
+            listaAutos:= aux2;
+        end
+        else
+            while (aux2 <> nil) do begin
+                
+            end;
+    end;
+end;
 var
     testViaje: Viaje;
+    testLista: ListaViajes;
+    testAutos: ListaAutoKm;
 begin
+    testLista:= nil;
     repeat
         write('numero viaje: ');
         readln(testViaje.nroViaje);
@@ -29,6 +78,10 @@ begin
             readln(testViaje.direccionDestino);
             write('kilometros recorridos: ');
             readln(testViaje.kms);
+
+            cargarViajes(testLista, testViaje);
         end;
     until (testViaje.nroViaje = 0);
+    testAutos:= nil;
+    autosQueMasRecorrieron(testLista, testAutos)
 end.
